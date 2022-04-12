e71131454daabe12247e252adf15e6e25a34f357
Anthony Merlier <46730440+ideosconsor@users.noreply.github.com>
2022-04-12 11:37:40 +0200

    Add correction

--- M11005-1.php
+++ M11005-1.php
@@ -1,16 +1,33 @@
+<?php
+// Déclarations de variables
+$str = "Hello World";
+$strUpper = strtoupper($str); // Version majuscule = HELLO WORLD
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>M11005-1 : Les variables</title>
+    <style>
+        .text-center {
+            text-align: center;
+        }
+
+        h1 {
+            color: #0a9396;
+        }
+    </style>
 </head>
 
 <body>
     <div>
         <a href="/">Retour à l'index</a>
+    </div>
+    <div class="text-center">
+        <h1><?php echo $strUpper; ?></h1>
     </div>
 </body>
 
--- M11005-3.php
+++ M11005-3.php
@@ -1,3 +1,26 @@
+<?php
+// On déclare ici nos variables
+$tv = [
+    "ref" => "47LG1000",
+    "name" => "téléviseur LED de 47",
+    "brand" => "LG",
+    "price" => 499,
+    "stock" =>  [
+        "Paris" => 14,
+        "Lyon" => 7,
+    ]
+];
+$robot = [
+    "ref" => "ROB4000",
+    "name" => "robot cuisine",
+    "brand" => "KENWOOD",
+    "price" => 679,
+    "stock" =>  [
+        "Paris" => 0,
+        "Lyon" => 21,
+    ]
+];
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -6,11 +29,64 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>M11005-3 : Les conditions</title>
+    <style>
+        .red {
+            color: red
+        }
+    </style>
 </head>
 
 <body>
     <div>
         <a href="/">Retour à l'index</a>
+    </div>
+    <div>
+
+        <!-- PRODUIT 1 -->
+        <div>
+            <h2><?php echo $tv["name"]; ?></h2>
+            <p>Stock à Paris :
+                <?php
+                if ($tv["stock"]["Paris"] === 0) {
+                    echo "<span class='red'>Non disponible</span>";
+                } else {
+                    echo $tv["stock"]["Paris"];
+                }
+                ?>
+            </p>
+            <p>Stock à Lyon :
+                <?php
+                if ($tv["stock"]["Lyon"] === 0) {
+                    echo "<span class='red'>Non disponible</span>";
+                } else {
+                    echo $tv["stock"]["Lyon"];
+                }
+                ?>
+            </p>
+        </div>
+
+        <!-- PRODUIT 2 -->
+        <div>
+            <h2><?php echo $robot["name"]; ?></h2>
+            <p>Stock à Paris :
+                <?php
+                if ($robot["stock"]["Paris"] === 0) {
+                    echo "<span class='red'>Non disponible</span>";
+                } else {
+                    echo $robot["stock"]["Paris"];
+                }
+                ?>
+            </p>
+            <p>Stock à Lyon :
+                <?php
+                if ($robot["stock"]["Lyon"] === 0) {
+                    echo "<span class='red'>Non disponible</span>";
+                } else {
+                    echo $robot["stock"]["Lyon"];
+                }
+                ?>
+            </p>
+        </div>
     </div>
 </body>
 
